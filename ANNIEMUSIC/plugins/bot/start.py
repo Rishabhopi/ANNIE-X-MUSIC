c20c33caf3c384c0ca347d12b99058b208d25b4d
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-08-12 08:31:22 +0530

    Update start.py

--- ANNIEMUSIC/plugins/bot/start.py
+++ ANNIEMUSIC/plugins/bot/start.py
@@ -31,7 +31,7 @@ ANNIE_VID = [
     "https://graph.org/file/2a7f857f31b32766ac6fc.mp4",
     "https://graph.org/file/7c1aa59649fbf3ab422da.mp4",
     "https://graph.org/file/318eac81e3d4667edcb77.mp4",
-"https://graph.org/file/82fd67aa56eb1b299e08d.mp4",
+     "https://graph.org/file/82fd67aa56eb1b299e08d.mp4",
 "https://graph.org/file/83ebf52e8bbf138620de7.mp4",
 "https://graph.org/file/ba7699c28dab379b518ca.mp4"
 ]
